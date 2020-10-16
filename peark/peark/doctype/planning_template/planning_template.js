e1e258b57018981a3b5b0d7a6db2e5a7be0518a8
Yefri Tavarez <yefritavarez@gmail.com>
2020-10-16 19:03:48 -0400

    Introduced Planing template

---
+++ peark/peark/doctype/planning_template/planning_template.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Yefri Tavarez Nolasco and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Planning Template', {
+	// refresh: function(frm) {
+
+	// }
+});
