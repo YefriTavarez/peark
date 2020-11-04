476c7f52fb90146836a9236629a7d2f46c6acd8b
Yefri Tavarez <yefritavarez@gmail.com>
2020-11-04 07:12:14 -0400

    Modify Payment Entry Workflow

---
+++ peark/controllers/erpnext/payment_entry.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Yefri Tavarez and contributors
+# For license information, please see license.txt
+
+import frappe
+
+from frappe import db as database
+
+
+def validate(doc, method=None):
+    if doc.payment_type != "Receive":
+        return False
+
+    if doc.is_new():
+        return False
+
+    if doc.db_get("workflow_state") == "Borrador" \
+            and doc.workflow_state == "Pendiente":
+        doc.workflow_state = "Aprobado"
