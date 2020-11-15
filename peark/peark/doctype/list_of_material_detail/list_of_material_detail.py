1c568d69224cb5ca4ac5b16d8030c374265beb8c
Yefri Tavarez <yefritavarez@gmail.com>
2020-11-14 22:32:09 -0400

    Now List of Material Detail can have its own name and can chosen individually without a List of Material

--- peark/peark/doctype/list_of_material_detail/list_of_material_detail.py
+++ peark/peark/doctype/list_of_material_detail/list_of_material_detail.py
@@ -7,5 +7,47 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
+from frappe.utils import cstr
+from frappe.model.naming import make_autoname
+
+
 class ListofMaterialDetail(Document):
-	pass
+    def autoname(self):
+        self.set_name()
+
+    def set_name(self):
+        naming_selection = "Naming Series"
+        naming_series = "LIST-OF-MATERIAL-DETAIL-"
+
+        if not self.naming_selection:
+            self.naming_selection = naming_selection
+
+        newname = None
+        if self.naming_selection == naming_selection:
+            if not self.naming_series:
+                self.naming_series = naming_series
+
+            newname = make_autoname(self.naming_series)
+
+        if self.naming_selection == "Supply Specs":
+            newname = cstr(self.supply_specs)
+
+        if not newname:
+            newname = make_autoname(naming_series)
+
+        self.name = newname
+
+    supply_specs = None
+    override_item_name = False
+    naming_selection = None
+    naming_series = None
+    qty = 0
+    fixed_qty = False
+    uom = None
+    last_purchase_rate_based_on = None
+    item = None
+    item_group = None
+    item_set_display = None
+    last_purchase_item = None
+    last_purchase_rate = .000
+    last_purchase_uom = None
