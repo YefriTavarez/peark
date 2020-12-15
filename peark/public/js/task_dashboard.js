5fd014f0e740c725f12bc6f33b26c24f126bf354
Yefri Tavarez <yefritavarez@gmail.com>
2020-12-15 16:33:59 -0400

    Redesign for Task Center

--- peark/public/js/task_dashboard.js
+++ peark/public/js/task_dashboard.js
@@ -16,49 +16,6 @@ peark.TaskDashboard = Class.extend({
 		this.content = jQuery(frappe.render_template('task_dashboard')).appendTo(this.parent);
 		this.result = this.content.find('.result');
 
-		// this.content.on('click', '.btn-move', function () {
-		// 	handle_move_add(jQuery(this), "Move")
-		// });
-
-		// this.content.on('click', '.btn-add', function () {
-		// 	handle_move_add(jQuery(this), "Add")
-		// });
-
-		// function handle_move_add(element, action) {
-		// 	let item = unescape(element.attr('data-item'));
-		// 	let warehouse = unescape(element.attr('data-warehouse'));
-		// 	let actual_qty = unescape(element.attr('data-actual_qty'));
-		// 	let disable_quick_entry = Number(unescape(element.attr('data-disable_quick_entry')));
-		// 	let entry_type = action === "Move" ? "Material Transfer" : null;
-
-		// 	if (disable_quick_entry) {
-		// 		open_stock_entry(item, warehouse, entry_type);
-		// 	} else {
-		// 		if (action === "Add") {
-		// 			let rate = unescape(jQuery(this).attr('data-rate'));
-		// 			peark.move_item(item, null, warehouse, actual_qty, rate, function () { me.refresh(); });
-		// 		}
-		// 		else {
-		// 			peark.move_item(item, warehouse, null, actual_qty, null, function () { me.refresh(); });
-		// 		}
-		// 	}
-		// }
-
-		// function open_stock_entry(item, warehouse, entry_type) {
-		// 	frappe.model.with_doctype('Stock Entry', function () {
-		// 		let doc = frappe.model.get_new_doc('Stock Entry');
-		// 		if (entry_type) {
-		// 			doc_entry_type = entry_type;
-		// 		}
-
-		// 		var row = frappe.model.add_child(doc, 'items');
-		// 		row.item_code = item;
-		// 		row.s_warehouse = warehouse;
-
-		// 		frappe.set_route('Form', doc.doctype, doc.name);
-		// 	});
-		// }
-
 		// more
 		this.content.find('.btn-more').on('click', function () {
 			me.start += 20;
@@ -110,7 +67,7 @@ peark.TaskDashboard = Class.extend({
 		if (context.data.length > 0) {
 			jQuery(frappe.render_template('task_dashboard_list', context)).appendTo(this.result);
 		} else {
-			var message = __(" Currently no stock available in any warehouse")
+			var message = __("Not found Task")
 			jQuery("<span class='text-muted small'>" + message + "</span>").appendTo(this.result);
 		}
 
@@ -145,7 +102,7 @@ peark.TaskDashboard = Class.extend({
 		const self = this;
 
 		jQuery(parent)
-			.find("button.btn-action")
+			.find("input[type=checkbox][data-action]")
 			.click(function (event) {
 				const doctype = jQuery(this)
 					.attr("data-doctype");
@@ -168,97 +125,4 @@ peark.TaskDashboard = Class.extend({
 				frappe.call({ method, args, callback });
 			});
 	}
-})
-
-peark.move_item = function (item, source, target, actual_qty, rate, callback) {
-	var dialog = new frappe.ui.Dialog({
-		title: target ? __('Add Item') : __('Move Item'),
-		fields: [
-			{
-				fieldname: 'item_code', label: __('Item'),
-				fieldtype: 'Link', options: 'Item', read_only: 1
-			},
-			{
-				fieldname: 'source', label: __('Source Warehouse'),
-				fieldtype: 'Link', options: 'Warehouse', read_only: 1
-			},
-			{
-				fieldname: 'target', label: __('Target Warehouse'),
-				fieldtype: 'Link', options: 'Warehouse', reqd: 1
-			},
-			{
-				fieldname: 'qty', label: __('Quantity'), reqd: 1,
-				fieldtype: 'Float', description: __('Available {0}', [actual_qty])
-			},
-			{ fieldname: 'rate', label: __('Rate'), fieldtype: 'Currency', hidden: 1 },
-		],
-	})
-	dialog.show();
-	dialog.get_field('item_code').set_input(item);
-
-	if (source) {
-		dialog.get_field('source').set_input(source);
-	} else {
-		dialog.get_field('source').df.hidden = 1;
-		dialog.get_field('source').refresh();
-	}
-
-	if (rate) {
-		dialog.get_field('rate').set_value(rate);
-		dialog.get_field('rate').df.hidden = 0;
-		dialog.get_field('rate').refresh();
-	}
-
-	if (target) {
-		dialog.get_field('target').df.read_only = 1;
-		dialog.get_field('target').value = target;
-		dialog.get_field('target').refresh();
-	}
-
-	dialog.set_primary_action(__('Submit'), function () {
-		var values = dialog.get_values();
-		if (!values) {
-			return;
-		}
-		if (source && values.qty > actual_qty) {
-			frappe.msgprint(__('Quantity must be less than or equal to {0}', [actual_qty]));
-			return;
-		}
-		if (values.source === values.target) {
-			frappe.msgprint(__('Source and target warehouse must be different'));
-		}
-
-		frappe.call({
-			method: 'peark.doctype_entry_entry_utils.make_stock_entry',
-			args: values,
-			freeze: true,
-			callback: function (r) {
-				frappe.show_alert(__('Stock Entry {0} created',
-					['<a href="#Form/Stock Entry/' + r.message.name + '">' + r.message.name + '</a>']));
-				dialog.hide();
-				callback(r);
-			},
-		});
-	});
-
-	jQuery('<p style="margin-left: 10px;"><a class="link-open text-muted small">'
-		+ __("Add more items or open full form") + '</a></p>')
-		.appendTo(dialog.body)
-		.find('.link-open')
-		.on('click', function () {
-			frappe.model.with_doctype('Stock Entry', function () {
-				var doc = frappe.model.get_new_doc('Stock Entry');
-				doc.from_warehouse = dialog.get_value('source');
-				doc.to_warehouse = dialog.get_value('target');
-				var row = frappe.model.add_child(doc, 'items');
-				row.item_code = dialog.get_value('item_code');
-				row.f_warehouse = dialog.get_value('target');
-				row.t_warehouse = dialog.get_value('target');
-				row.qty = dialog.get_value('qty');
-				row.conversion_factor = 1;
-				row.transfer_qty = dialog.get_value('qty');
-				row.basic_rate = dialog.get_value('rate');
-				frappe.set_route('Form', doc.doctype, doc.name);
-			})
-		});
-}
+});
