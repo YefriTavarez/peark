fdb04091870b74dc0413cc963dc31f2b32ea3541
Yefri Tavarez <yefritavarez@gmail.com>
2021-02-03 09:21:43 -0400

    Enable system to create a Payment Entry from Payment Entry Request

--- peark/peark/doctype/payment_entry_request/payment_entry_request.js
+++ peark/peark/doctype/payment_entry_request/payment_entry_request.js
@@ -3,7 +3,10 @@
 
 frappe.ui.form.on('Payment Entry Request', {
 	refresh(frm) {
-		frm.trigger("update_currency_labels");
+		frappe.run_serially([
+			() => frm.trigger("add_custom_buttons"),
+			() => frm.trigger("update_currency_labels"),
+		]);
 	},
 	party_type(frm) {
 		const fieldname = "party";
@@ -26,10 +29,75 @@ frappe.ui.form.on('Payment Entry Request', {
 			frm.call("set_party_name");
 		}
 	},
+	add_custom_buttons(frm) {
+		frappe.run_serially([
+			() => frm.trigger("add_create_payment_entry_button"),
+		]);
+	},
+	add_create_payment_entry_button(frm) {
+		const parent = __("Create");
+		const label = __("Payment Entry");
+		const action = event => {
+			frm.trigger("create_payment_entry");
+		};
+
+		frm.add_custom_button(label, action, parent);
+	},
+	create_payment_entry(frm) {
+		const { doc } = frm;
+
+		const doctype = "Payment Entry";
+		const newdoc = frappe
+			.model
+			.get_new_doc(doctype);
+
+		const {
+			party_type,
+			party,
+			party_name,
+			amount,
+			currency,
+			reason,
+			exchange_rate,
+		} = doc;
+
+		const args = {
+			party_type: party_type,
+			party: party,
+			party_name: party_name,
+			paid_amount: amount,
+			received_amount: amount,
+			currency: currency,
+			reason: reason,
+			source_exchange_rate: exchange_rate,
+			target_exchange_rate: exchange_rate,
+			// references: [{}]
+		};
+
+		// update args with defaults
+		Object.assign(args, {
+			payment_type: "Pay",
+			company: frappe.boot.sysdefaults.company,
+			posting_date: frappe.datetime.get_today(),
+			naming_series: "SAL-PAGO-",
+		});
+
+		const route = new Array("Form");
+		(route => {
+			route.push(newdoc.doctype);
+			route.push(newdoc.name);
+		})(route);
+
+		frappe.set_route(route)
+			.then(() => {
+				Object.assign(newdoc, args);
+				frm.refresh();
+			});
+	},
 	update_currency_labels(frm) {
 		const { doc } = frm;
 
-		const fields = [ "amount" ];
+		const fields = ["amount"];
 
 		frm.set_currency_labels(fields, doc.currency);
 	},
